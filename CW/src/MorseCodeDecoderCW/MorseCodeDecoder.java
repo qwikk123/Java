877826bb303736af075de037c47d07fa7c99aec1
qwikk <44299295+qwakk@users.noreply.github.com>
2023-01-21 13:04:27 +0100

    Completed MorseCodeDecoder

--- CW/src/MorseCodeDecoderCW/MorseCodeDecoder.java
+++ CW/src/MorseCodeDecoderCW/MorseCodeDecoder.java
@@ -3,6 +3,15 @@ package MorseCodeDecoderCW;
 public class MorseCodeDecoder {
     public static String decode(String morseCode) {
         // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
-        return "";
+        String[] words = morseCode.trim().split(" {3}");
+        StringBuilder sb = new StringBuilder();
+        for (String s : words) {
+            String[] chars = s.split(" ");
+            for (String c : chars) {
+                sb.append(MorseCode.get(c));
+            }
+            sb.append(" ");
+        }
+        return sb.toString().trim();
     }
 }
