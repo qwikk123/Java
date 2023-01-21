1e8a239cfb91f69abe4d49787df196117ef6726c
qwikk <44299295+qwakk@users.noreply.github.com>
2023-01-21 16:32:45 +0100

    Completed LoopInspector

--- CW/src/CanYouGetTheLoopCW/LoopInspector.java
+++ CW/src/CanYouGetTheLoopCW/LoopInspector.java
@@ -1,7 +1,24 @@
 package CanYouGetTheLoopCW;
 
+import java.util.HashMap;
+
 public class LoopInspector {
     public int loopSize(Node node) {
-        return 0;
+        // Use the `getNext()` method to get the following node.
+        HashMap<Node, Integer> nodeMap = new HashMap<Node, Integer>();
+        int count = 0;
+        nodeMap.put(node, count);
+        count++;
+        Node next = node.getNext();
+        while (next != null) {
+            if (nodeMap.containsKey(next)) {
+                count -= nodeMap.get(next);
+                break;
+            }
+            nodeMap.put(next, count);
+            next = next.getNext();
+            count++;
+        }
+        return count;
     }
 }
