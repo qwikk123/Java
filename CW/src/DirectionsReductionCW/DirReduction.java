50294fe7a9c0e3a491a6452acd9aa800eed9c9a5
qwikk <44299295+qwakk@users.noreply.github.com>
2023-01-21 16:16:34 +0100

    Final refactor of DirReduction

--- CW/src/DirectionsReductionCW/DirReduction.java
+++ CW/src/DirectionsReductionCW/DirReduction.java
@@ -15,9 +15,7 @@ public class DirReduction {
         ArrayList<String> tempList = new ArrayList<>(Arrays.asList(arr));
 
         for (int i = tempList.size()-2, j = tempList.size()-1; i>=0; j--, i--) {
-            Integer dir1 = valueMap.get(tempList.get(i));
-            Integer dir2 = valueMap.get(tempList.get(j));
-            if (isBad(dir1,dir2)) {
+            if (valueMap.get(tempList.get(i)) + valueMap.get(tempList.get(j)) == 0) {
                 tempList.remove(j);
                 tempList.remove(i);
                 j = Math.min(tempList.size(), j+2);
@@ -26,8 +24,5 @@ public class DirReduction {
         }
 
         return tempList.toArray(new String[0]);
-    }
-    public static boolean isBad(Integer dir1, Integer dir2) {
-        return dir1+dir2==0;
     }
 }
