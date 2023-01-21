d7acdd169b82dcef579ef482b8a252e5df5be876
qwikk <44299295+qwakk@users.noreply.github.com>
2023-01-21 12:34:18 +0100

    Added MorseCodeDecoder(empty)

---
+++ CW/src/MorseCodeDecoderCW/MorseCodeDecoder.java
@@ -0,0 +1,8 @@
+package MorseCodeDecoderCW;
+
+public class MorseCodeDecoder {
+    public static String decode(String morseCode) {
+        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
+        return "";
+    }
+}
