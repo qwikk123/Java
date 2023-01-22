d58906eb3e877d67344beda36426bfddecde7290
qwikk <44299295+qwakk@users.noreply.github.com>
2023-01-22 22:07:27 +0100

    Refactored PyramidSlideDownCW

--- CW/src/PyramidSlideDownCW/LongestSlideDown.java
+++ CW/src/PyramidSlideDownCW/LongestSlideDown.java
@@ -8,17 +8,9 @@ public class LongestSlideDown {
             maxValues[i] = pyramid[i].clone();
         }
         for (int i = pyramid.length-1; i > 0; i--) {
-            for (int j = 0; j < pyramid[i].length; j++) {
-                if (j == 0) {
-                    maxValues[i-1][j] += maxValues[i][j];
-                }
-                else if (j == pyramid[i-1].length) {
-                    maxValues[i-1][j-1] = Math.max(maxValues[i-1][j-1], maxValues[i][j]+pyramid[i-1][j-1]);
-                }
-                else {
-                    maxValues[i-1][j-1] = Math.max(maxValues[i-1][j-1], maxValues[i][j]+pyramid[i-1][j-1]);
-                    maxValues[i-1][j] = Math.max(maxValues[i-1][j], maxValues[i][j]+pyramid[i-1][j]);
-                }
+            for (int j = 0; j < pyramid[i].length-1; j++) {
+                maxValues[i-1][j] += Math.max(maxValues[i][j], maxValues[i][j+1]);
+
             }
         }
         return maxValues[0][0];
