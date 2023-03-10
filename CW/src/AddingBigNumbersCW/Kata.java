ce190e41780400c4eabf5f39fb24d288b309ac48
qwikk <44299295+qwakk@users.noreply.github.com>
2023-03-10 15:57:20 +0100

    Refactored AddingBigNumber, removed duplicate code

--- CW/src/AddingBigNumbersCW/Kata.java
+++ CW/src/AddingBigNumbersCW/Kata.java
@@ -7,9 +7,9 @@ public class Kata {
         Integer rest = 0;
         int currentA = a.length()-1, currentB = b.length()-1;
 
-        while (currentA >= 0 && currentB >= 0) {
-            Integer intA = Integer.parseInt(Character.toString(a.charAt(currentA)));
-            Integer intB = Integer.parseInt(Character.toString(b.charAt(currentB)));
+        while (currentA >= 0 || currentB >= 0) {
+            Integer intA = (currentA >= 0) ? Integer.parseInt(Character.toString(a.charAt(currentA))) : 0;
+            Integer intB = (currentB >= 0) ? Integer.parseInt(Character.toString(b.charAt(currentB))) : 0;
             Integer newNumber = intA+intB+rest;
             rest = 0;
             if (newNumber >= 10) {
@@ -21,35 +21,6 @@ public class Kata {
             }
 
             currentA--; currentB--;
-        }
-        while (currentA >= 0) {
-            Integer intA = Integer.parseInt(Character.toString(a.charAt(currentA)));
-            Integer newNumber = intA+rest;
-            rest = 0;
-            if (newNumber >= 10) {
-                newNumber -= 10;
-                sb.append(newNumber);
-                rest = 1;
-            } else {
-                sb.append(newNumber);
-            }
-
-            currentA--;
-        }
-        while (currentB >= 0) {
-            Integer intB = Integer.parseInt(Character.toString(b.charAt(currentB)));
-            Integer newNumber = intB+rest;
-            System.out.println(newNumber);
-            rest = 0;
-            if (newNumber >= 10) {
-                newNumber -= 10;
-                sb.append(newNumber);
-                rest = 1;
-            } else {
-                sb.append(newNumber);
-            }
-
-            currentB--;
         }
         if(rest == 1 && a.length() == b.length()) sb.append(rest);
 
