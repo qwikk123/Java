3d506af1c78d92a08d0da9a53d85470f31ce37af
qwikk <44299295+qwakk@users.noreply.github.com>
2023-04-20 15:01:25 +0200

    Refactor

--- CW/src/BouncingBall/BouncingBall.java
+++ CW/src/BouncingBall/BouncingBall.java
@@ -5,9 +5,7 @@ import java.util.stream.Stream;
 public class BouncingBall {
 
     public static int bouncingBall(double h, double bounce, double window) {
-        if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1;
-        return (int) Stream.iterate(h, x -> x*bounce)
-                .takeWhile(x -> x > window)
-                .count()*2-1;
+        return (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) ? -1 :
+                (int) Stream.iterate(h, x -> x > window, x -> x*bounce).count()*2-1;
     }
 }
