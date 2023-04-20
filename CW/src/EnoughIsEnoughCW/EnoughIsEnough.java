035c451708ccbe3fed43a591d61b1c9dfad6e05d
qwikk <44299295+qwakk@users.noreply.github.com>
2023-04-20 16:15:40 +0200

    Added EnoughIsEnough

---
+++ CW/src/EnoughIsEnoughCW/EnoughIsEnough.java
@@ -0,0 +1,22 @@
+package EnoughIsEnoughCW;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class EnoughIsEnough {
+
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        LinkedList<Integer> intList = new LinkedList<>();
+        for (int i : elements) {
+            map.computeIfPresent(i, (k,v) -> v+1);
+            map.putIfAbsent(i,1);
+            if (map.get(i) <= maxOccurrences) {
+                intList.add(i);
+            }
+        }
+        return intList.stream().mapToInt(x -> x).toArray();
+    }
+
+}
